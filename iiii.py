0856bfc348f82fec9c728cf350b27aa5c564f545
phattnachi <phattnachi@gmail.com>
2026-01-06 15:18:57 +0700

    เพิ่ม mehod 3 ตว

--- iiii.py
+++ iiii.py
@@ -1,3 +1,20 @@
 class BackAccount:
     def __init__(self):
         self.__balance = 0
+
+    def deposit(self, amount):
+        if amount >0:
+            self,__balance += amount
+            print(f'ฝากสำเร็จ: {amount} บาท')
+        else:
+            print('ยอดเงินต้องมากกว่า 0')
+
+        def withdraw(self, amount):
+            if amount > 0 and amount <= self.__balance:
+                self.__balance -= amount
+                print(f'ถอนเงินสำเร็จ: {amount} บาท')
+            else:
+                print(f'ถอนไม่สำเร็จ ! (ยอดเงินไม่เพียงพอ: {amount})')
+
+            def check_balance(self):
+                return self.__balance
